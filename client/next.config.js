030addf72e9c3a387f3998d6bbf67ed60a4dbb65
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2024-11-20 17:53:08 +0530

    config files

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    webpack: (config) => {
+      config.experiments = {
+        ...config.experiments,
+        asyncWebAssembly: true,
+        topLevelAwait: true,
+        layers: true,
+      };
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
