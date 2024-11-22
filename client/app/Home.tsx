1bfe3683a8a133fd08c0867bb361a3054a68217b
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2024-11-22 22:03:44 +0530

    home.tsx lucid removed

--- client/app/Home.tsx
+++ client/app/Home.tsx
@@ -1,21 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Dashboard from "@/components/Dashboard";
 
-import { Address, Blockfrost, Lucid, LucidEvolution, Network } from "@lucid-evolution/lucid";
-
 export default function Home() {
-  const NETWORK = process.env.NEXT_PUBLIC_CARDANO_NETWORK as Network;
-  const BF_URL = `${process.env.NEXT_PUBLIC_BF_URL}`;
-  const BF_PID = `${process.env.NEXT_PUBLIC_BF_PID}`;
-  const BLOCKFROST = new Blockfrost(BF_URL, BF_PID);
-
-  const [lucid, setLucid] = useState<LucidEvolution>();
   const [result, setResult] = useState("");
-
-  useEffect(() => {
-    Lucid(BLOCKFROST, NETWORK).then(setLucid).catch(handleError);
-  }, []);
-
 
   function handleError(error: any) {
     const { info, message } = error;
@@ -52,18 +39,16 @@ export default function Home() {
    * eg. empty address == not connected; has address == connected;
    * @param wallet
    */
-  
+
   //#endregion
 
   return (
     <>
       <div className="flex justify-center overflow-hidden">
         <div className="flex flex-col gap-2 overflow-hidden">
-          {lucid ? (
-            <Dashboard onError={handleError} setActionResult={setResult} />
-          ) : (
-            <span className="uppercase">Initializing Lucid</span>
-          )}
+
+          <Dashboard onError={handleError} setActionResult={setResult} />
+
           <span className="font-mono break-words whitespace-pre-wrap">{result}</span>
         </div>
       </div>
