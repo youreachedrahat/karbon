1bb6e15a61f9229766a0d35a61fdbe7fe3f3d624
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2024-11-25 17:54:31 +0530

    validator contract mint

--- aikenbuild.sh
+++ aikenbuild.sh
@@ -1,2 +1,4 @@
 aiken build -t verbose
 aiken blueprint convert -v identification_nft.identification_nft.mint > ./client/components/compiled/identification_nft.json
+aiken blueprint convert -v validator_contract.validator_contract.spend > ./client/components/compiled/validator_contractSpend.json
+aiken blueprint convert -v validator_contract.validator_contract.mint > ./client/components/compiled/validator_contractMint.json
